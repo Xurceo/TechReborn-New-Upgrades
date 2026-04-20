e37476e47ac169b80d5ced673e617e71bcf5f9bb
Xurceo <shnalpavel2019@gmail.com>
2026-04-20 18:08:35 +0300

    Fabric template mod

---
+++ src/client/java/trnewupgrades/client/TechRebornNewUpgradesClient.java
@@ -0,0 +1,10 @@
+package trnewupgrades.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechRebornNewUpgradesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
